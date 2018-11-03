3c1ad611938d695a3910aa578b412f08121886cf
MisakiFx <1761607418@qq.com>
2018-11-03 21:07:38 +0800

    2017C语言练习题及代码

--- 计算机作业/3月19/本息和.cpp
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#define INTEREST 0.025
-#define TAX 0.2
-int main()
-{
-	double deposit = 1000;
-	double interest = 1000 * INTEREST * 1 * (1 - TAX);
-	printf("%.2lf",deposit + interest);
-} 
