07dc2b1ce1fcdfe2e01adf8f8d0444f32093c957
MisakiFx <1761607418@qq.com>
2019-01-01 01:32:04 +0800

    2019.1.1

--- C-third/第十四次作业/第十四次作业.cpp
+++ C-third/第十四次作业/第十四次作业.cpp
@@ -1,5 +1,23 @@
 #include <stdio.h>
+#include <string.h>
+char* rotate(char* str, int k)
+{
+    int len = strlen(str);
+    char str1[len + 1] = {0};
+    str1[len] = '\0';
+    for(int i = k; i < len; i++)
+    {
+        str1[i - k] = str[i];
+    }
+    for(int i = len - k, j = 0; i < len; i++)
+    {
+        str1[i] = str[j++];
+    }
+    printf("%s的第%d个字母的反转为%s\n",str,k,str1);    
+}
 int main()
 {
-    
+    char* A = "ABCD";
+    rotate(A, 1);
+    rotate(A, 2);
 }
