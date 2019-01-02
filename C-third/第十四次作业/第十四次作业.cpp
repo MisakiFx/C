169f31f75dd0d4a7b5be2d902653254733a197b8
MisakiFx <1761607418@qq.com>
2019-01-02 23:31:31 +0800

    2019.1.2

--- C-third/第十四次作业/第十四次作业.cpp
+++ C-third/第十四次作业/第十四次作业.cpp
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 char* rotate(char* str, int k)
 {
     int len = strlen(str);
-    char str1[len + 1] = {0};
+    char* str1 = (char*)malloc(sizeof(char) * (len + 1));
     str1[len] = '\0';
     for(int i = k; i < len; i++)
     {
@@ -13,11 +14,30 @@ char* rotate(char* str, int k)
     {
         str1[i] = str[j++];
     }
-    printf("%s的第%d个字母的反转为%s\n",str,k,str1);    
+    return str1;
+    //printf("%s的第%d个字母的反转为%s\n",str,k,str1);    
+}
+int isRotate(char* str1, char* str2)
+{
+    int len1 = strlen(str1);
+    int len2 = strlen(str2);
+    if(len1 != len2) return 0;
+    for(int i = 0; i < len1; i++)
+    {
+        char* t = rotate(str1, i);
+        if(strcmp(t, str2) == 0)
+        {
+            return 1;
+        }
+    }
+    return 0;
 }
 int main()
 {
     char* A = "ABCD";
-    rotate(A, 1);
-    rotate(A, 2);
+    char* a1 = rotate(A, 1);
+    char* a2 = rotate(A, 2);
+    printf("%s向左旋转一个字符为%s\n", A, a1);
+    printf("%s想做旋转两个字符为%s\n", A, a2);
+    printf("%s是否为%s旋转后的结果：%d", A, a2, isRotate(A, a2));
 }
