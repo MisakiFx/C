ed4e1b079f99751461ccac854e0c7470df5c3507
MisakiFx <1761607418@qq.com>
2019-01-19 03:32:32 +0800

    2019.1.19

--- C-third/第十六次作业/第十六次作业.cpp
+++ C-third/第十六次作业/第十六次作业.cpp
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <assert.h>
+#include <stdint.h>
 /*
 模拟实现strcpy()
 **/
@@ -36,11 +38,138 @@ int Strcat(char* str1, char* str2)
     }
     return 1;
 }
+/*
+模拟实现strstr()
+*/
+const char* Strstr(const char* str1, const char* str2)
+{
+    assert(str1 != NULL);
+    assert(str2 != NULL);
+    if(*str1 == '\0' || *str2 == '\0')
+    {
+        return NULL;
+    }
+    const char* black_ptr = str1;
+    while(*black_ptr != '\0')
+    {
+        const char* red_ptr = black_ptr;
+        const char* sub_ptr = str2;
+        while(*red_ptr != '\0' && *sub_ptr != '\0' && (*red_ptr == *sub_ptr))
+        {
+            ++red_ptr;
+            ++sub_ptr;
+        }
+        if(*sub_ptr == '\0')
+        {
+            //找到了
+            return black_ptr;    
+        }
+        ++black_ptr;
+    }
+    //没找到
+    return NULL;
+}
+/*
+模拟实现strchr()
+*/
+const char* Strchr(const char* str1, int character)
+{
+    const char* p = str1;
+    while(*p != '\0' && *p != character)
+    {
+        p++; 
+    }
+    if(*p == '\0')
+    {
+        return NULL;
+    }
+    return p;
+}
+/*
+模拟实现strcmp()
+*/
+int Strcmp(const char* str1, const char* str2)
+{
+    assert(str1 != NULL);
+    assert(str2 != NULL);
+    while(*str1 != '\0' && *str2 != '\0')
+    {
+        if(*str1 < *str2)
+        {
+            return -1;
+        }
+        else if(*str1 > *str2)
+        {
+            return 1;
+        }
+        else 
+        {
+            ++str1;
+            ++str2;
+        }
+    }
+    if(*str1 == '\0' && *str2 != '\0')
+    {
+        return -1;
+    }
+    if(*str1 != '\0' && *str2 == '\0')
+    {
+        return 1;
+    }
+    if(*str1 == '\0' && *str2 == '\0')
+    {
+        return 0;
+    }
+}
+/*
+模拟实现memcpy()
+*/
+void* Memcpy(void* dest, const void* src, size_t num)
+{
+    assert(dest != NULL);
+    assert(src != NULL);
+    char* pdest = (char*)dest;
+    char* psrc = (char*)src;
+    for(size_t i = 0; i < num; i++)
+    {
+        pdest[i] = psrc[i];
+    }
+    return dest;
+}
+/*
+模拟实现memmove()
+*/
+void* Memmove(void* dest, const void* src, size_t num)
+{
+    assert(dest != NULL);
+    assert(src != NULL);
+    char* pdest = (char*)dest;
+    char* psrc = (char*)src;
+    if(pdest > psrc && pdest < psrc + num)
+    {
+        for(int64_t i = num - 1; i >= 0; i--)
+        {
+            pdest[i] = psrc[i];
+        }
+    }
+    else
+    {
+        Memcpy(dest, src, num);
+    }
+    return dest;
+}
 int main()
 {
-    char a[1024] = "abc";
-    char b[] = "abcdef";
+    char a[1024] = {0};
+    char b[] = "123abcdef";
     //Strcpy(a, b); 
-    Strcat(a, b);
-    printf("%s\n", a);
+    //Strcat(a, b);
+    //const char* p = Strstr(b, a);
+    //const char* p = Strchr(a, 'd');
+    //printf("%p\n", p);
+    //if(p != NULL)
+    //printf("%d\n", p - a + 1);
+    //else printf("NULL!");
+    Memmove(b + 1, b, 10);
+    printf("%s\n", b);
 }
